0cfa07fb371df1be412107d3b6c988a241c79299
norhizami.rashid <norhizami.rashid@sicmsb.com>
2018-04-23 22:58:37 +0800

    Initial Commit

---
+++ SingaporeWeatherApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SingaporeWeatherApp
+//
+//  Created by Mohammad Norhizami Bin Mohd Rashid on 23/04/2018.
+//  Copyright © 2018 Mohammad Norhizami Bin Mohd Rashid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
